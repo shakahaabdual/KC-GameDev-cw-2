996134362b690d04f4601c2b5f1fe1e846296338
shakah <alllshaikhah@gmail.com>
2022-07-30 22:36:50 +0300

    done

---
+++ shakah/Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    string heroName = " sweets";
+    int heroheight = 16 + 5;
+    int age = 15;
+    string suparpower = "run ";
+    string name = "saqer";
+    int agev = 9;
+    int height = 152;
+    string power = "none ";
+
+    // Start is called before the first frame updatec
+    void Start()
+    {
+        print("my hero name is " + heroName);
+        print("my heroheight is " + heroheight);
+        print("my hero age " + age);
+        print("my hero superpower is " + suparpower);
+        print("my villan name is " + name);
+        print("my villan age" + agev);
+        print("my villan height" + height);
+        print("my villan suparpower" + power);
+
+
+    }
+} 
+
